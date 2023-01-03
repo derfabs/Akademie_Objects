ee5ac83ccdb07ae97e80b3c3b81af116dc5f009f
salty-muffin <t.babel@gmx.com>
2023-01-03 14:30:22 +0100

    added libraries from adafruit

--- phone/code.py
+++ phone/code.py
@@ -1,11 +1,12 @@
 import board
 import time
 import simpleio
+import digitalio
+from adafruit_debouncer import Debouncer
 
-led = simpleio.DigitalOut(board.LED)
+led = simpleio.DigitalOut(board.D14)
+switch = Debouncer(simpleio.DigitalIn(board.D12, pull=digitalio.Pull.UP))
 
 while True:
-    led.value = True
-    time.sleep(0.5)
-    led.value = False
-    time.sleep(0.5)
+  switch.update()
+  led.value = not switch.value
