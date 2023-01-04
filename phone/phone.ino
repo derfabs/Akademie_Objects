6048b5ffd9d74f80635795894e443c2f3db69312
salty-muffin <t.babel@gmx.com>
2023-01-04 11:22:06 +0100

    simplified

--- phone/phone.ino
+++ phone/phone.ino
@@ -1,22 +1,16 @@
-// #include <SoftwareSerial.h>
+#include <HardwareSerial.h>
 #include <DFRobotDFPlayerMini.h>
 
-const uint8_t POT = 4;
-
-DFRobotDFPlayerMini sound;
 DFRobotDFPlayerMini noise;
-
-// SoftwareSerial serialPort; // RX, TX
 
 void setup()
 {
   Serial.begin(115200);
 
   Serial2.begin(9600);
-  // serialPort.begin(9600, SWSERIAL_8N1, 18, 19, false);
 
-  // initializing DFPlayer ... (may take 3~5 seconds)
-  if (!sound.begin(Serial2))
+  // initializing DFPlayer (may take 3~5 seconds)
+  if (!noise.begin(Serial2))
   {
     Serial.println("unable to begin");
 
@@ -25,22 +19,10 @@ void setup()
       delay(1);
     }
   }
-  Serial.println("sound mini online");
-  // if (!noise.begin(serialPort))
-  // {
-  //   Serial.println("unable to begin");
+  Serial.println("noise online");
 
-  //   while (true)
-  //   {
-  //     delay(1);
-  //   }
-  // }
-  Serial.println("noise mini online");
-
-  sound.volume(10); // set volume value. From 0 to 30
-  // noise.volume(10);
-  sound.play(1);    // play the first mp3
-  // noise.play(1);
+  noise.volume(10);  // set volume value. From 0 to 30
+  noise.randomAll(); // play random mp3
 }
 
 void loop()
@@ -50,11 +32,9 @@ void loop()
   if (millis() - timer > 10000)
   {
     timer = millis();
-    sound.next(); // play next mp3 every 10 second.
-    // noise.next();
+    noise.next(); // play next mp3 every 10 second.
+    Serial.println("play");
   }
 
   // other code here
-  Serial.println(analogRead(A10));
-  delay(100);
 }
