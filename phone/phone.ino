8b70c2aca315aaf3e138796229fd853aeba1a89c
salty-muffin <t.babel@gmx.com>
2023-01-04 15:26:11 +0100

    two working dfplayers!

--- phone/phone.ino
+++ phone/phone.ino
@@ -1,18 +1,30 @@
 #include <HardwareSerial.h>
 #include <DFRobotDFPlayerMini.h>
 
+DFRobotDFPlayerMini sound;
 DFRobotDFPlayerMini noise;
 
 void setup()
 {
   Serial.begin(115200);
 
+  Serial1.begin(9600, SERIAL_8N1, 32, 33);
   Serial2.begin(9600);
 
   // initializing DFPlayer (may take 3~5 seconds)
+  if (!sound.begin(Serial1))
+  {
+    Serial.println("unable to begin sound");
+
+    while (true)
+    {
+      delay(1);
+    }
+  }
+  Serial.println("sound online");
   if (!noise.begin(Serial2))
   {
-    Serial.println("unable to begin");
+    Serial.println("unable to begin noise");
 
     while (true)
     {
@@ -21,7 +33,9 @@ void setup()
   }
   Serial.println("noise online");
 
+  sound.volume(20);  // set volume value. From 0 to 30
   noise.volume(10);  // set volume value. From 0 to 30
+  sound.randomAll(); // play random mp3
   noise.randomAll(); // play random mp3
 }
 
@@ -32,6 +46,7 @@ void loop()
   if (millis() - timer > 10000)
   {
     timer = millis();
+    sound.next(); // play next mp3 every 10 second.
     noise.next(); // play next mp3 every 10 second.
     Serial.println("play");
   }
